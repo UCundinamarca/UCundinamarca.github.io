eb7da0dd54c2f57b41c97b5ab1351eadbafa6fda
William Arevalo <willi11-95@hotmail.com>
2017-08-12 15:42:41 +0000

    Updated 'js/database.js' via CloudCannon

--- js/database.js
+++ js/database.js
@@ -17,13 +17,11 @@ articles.on('value', function(ss){
 
 //Agregar elementos al HTML y llenarlos, agregar hijos de la tabla con child_added
 //acceso a una colección
-var ref = database.ref('respuestas');
-ref.addChildEventListener(new ChildEventListener() {
-    @Override
-    public void onChildAdded(DataSnapshot dataSnapshot, String prevChildKey) {
-        Post newPost = dataSnapshot.getValue(Post.class);
-        System.out.println("Author: " + newPost.author);
-        System.out.println("Title: " + newPost.title);
-        System.out.println("Previous Post ID: " + prevChildKey);
-    }
+var refRespuestas = database.ref('respuestas');
+refRespuestas.on('child_added', function(ss) {
+    var respuesta = ss.val();
+    console.log(respuesta);
+    var item = document.createElement('li');
+    item.innerHTML = '<input name="respuesta" type="radio" value="' + respuesta.valor + '"> ' + respuesta.respuesta;
+    getId('lista').appendChild(item);
 });
