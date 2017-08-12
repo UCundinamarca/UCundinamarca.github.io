d805ca1b1b7662ad1e6d34dc123736b5aec204d5
William Arevalo <willi11-95@hotmail.com>
2017-08-12 16:24:14 +0000

    Updated 'js/database.js' via CloudCannon

--- js/database.js
+++ js/database.js
@@ -2,16 +2,26 @@
 
 var database = firebase.database();
 
-var refRespuestas = database.ref("ask");
+//Accedo a un documento
+var articles = database.ref('articles');
 
-refRespuestas.on("child_added", function(ss) {
-    var respuesta = ss.val();
-    console.log("valor: " + respuesta.valor);
-    console.log("respuesta: " + respuesta.respuesta);
+//Accder a los datos internos de a uno con value
+articles.on('value', function(ss){
+    //Codigo  devuelve el valor de lo que busca
+    var article = ss.val();
+    console.log(article);
+    getId("autor").innerHTML = article.id.autor;
+    getId("titulo").innerHTML = article.id.titulo;
+    getId("link").innerHTML = article.id.link;
+    getId("fecha").innerHTML = article.id.fechaLanzado;
 });
 
+//Agregar elementos al HTML y llenarlos, agregar hijos de la tabla con child_added
 
-//var commentsRef = firebase.database().ref('post-comments/' + postId);
-//commentsRef.on('child_added', function(data) {
-//  addCommentElement(postElement, data.key, data.val().text, data.val().author);
-//});
+var refRespuestas = database.ref('ask');
+refRespuestas.on('child_added', function(ss) {
+   var respuesta = ss.val();
+   var item = document.createElement('li');
+   item.innerHTML = '<input name="respuesta" type="radio" value="' + respuesta.valor + '"> ' + respuesta.respuesta;
+   getId('lista').appendChild(item);
+});
