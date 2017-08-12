3a69edd24c9c0fb1ddd8b53d248ebd82b3af9e0c
William Arevalo <willi11-95@hotmail.com>
2017-08-12 15:27:18 +0000

    Updated 'js/database.js' via CloudCannon

--- js/database.js
+++ js/database.js
@@ -2,18 +2,7 @@
 
 var database = firebase.database();
 
-//Accedo a un documento
-var articles = database.ref('articles');
 
-//Accder a los datos internos de a uno con value
-articles.on('value', function(ss){
-    //Codigo  devuelve el valor de lo que busca
-    var article = ss.val();
-    getId("autor").innerHTML = article.id.autor;
-    getId("titulo").innerHTML = article.id.titulo;
-    getId("link").innerHTML = article.id.link;
-    getId("fecha").innerHTML = article.id.fechaLanzado;
-});
 
 //Agregar elementos al HTML y llenarlos, agregar hijos de la tabla con child_added
 //acceso a una colección
