d341b6d2516b320aea0eb1cf95b8c01c630c727c
William Arevalo <willi11-95@hotmail.com>
2017-08-12 18:43:13 +0000

    Updated 'js/auth.js' via CloudCannon

---
+++ js/auth.js
@@ -0,0 +1,15 @@
+//Accedo al servicio de autenticacion
+var auth = firebase.auth();
+
+//Login de usuarios
+getid('login').addEventListener('click', function(){
+    var provider = new firebase.auth.GoogleAuthProvider();
+    auth.signInWithPopup(provider);
+        .then( function (result) {
+            var user = result.user;
+            console.log("El usuario es ", user);
+        })
+        .catch(function (err) {
+            console.log("Hubo un error" + err);
+        })
+});
