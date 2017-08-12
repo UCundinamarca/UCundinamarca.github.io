ca1153f11c0927c0408c35e27ccd168e93cefa14
William Arevalo <willi11-95@hotmail.com>
2017-08-12 15:46:34 +0000

    Updated 'js/database.js' via CloudCannon

--- js/database.js
+++ js/database.js
@@ -18,8 +18,8 @@ articles.on('value', function(ss){
 //Agregar elementos al HTML y llenarlos, agregar hijos de la tabla con child_added
 //acceso a una colección
 var refRespuestas = database.ref('respuestas');
-refRespuestas.on('child_added', function(ss) {
-    var respuesta = ss.val()
+refRespuestas.on('child_added', function(data) {
+    var respuesta = data.val()
     console.log(respuesta);
     var item = document.createElement('li');
     item.innerHTML = '<input name="respuesta" type="radio" value="' + respuesta.valor + '"> ' + respuesta.respuesta;
