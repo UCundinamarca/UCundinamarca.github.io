d018f9afd6b1b51cb4b3d355b63b2a167556db12
William Arevalo <willi11-95@hotmail.com>
2017-08-12 17:45:20 +0000

    Updated 'js/database.js' via CloudCannon

--- js/database.js
+++ js/database.js
@@ -2,7 +2,15 @@
 
 var database = firebase.database();
 
+var articles = database.ref().child('articles').child('id');
 
+articles.on('value', function(ss){
+    var article = ss.val();
+    getId("autor").innerHTML = article.autor;
+    getId("titulo").innerHTML = article.titulo;
+    getId("link").innerHTML = article.link;
+    getId("fecha").innerHTML = article.fechaLanzado;
+});
 var refRespuestas = database.ref().child('respuestas');
 refRespuestas.on('child_added', function(ss) {
     var respuesta = ss.val();
