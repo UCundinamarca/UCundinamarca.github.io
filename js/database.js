c92b4a113c26dc5704dee0e4946690d2e27452f4
William Arevalo <willi11-95@hotmail.com>
2017-08-12 17:36:24 +0000

    Updated 'js/database.js' via CloudCannon

--- js/database.js
+++ js/database.js
@@ -11,4 +11,10 @@ articles.on('value', function(ss){
     getId("link").innerHTML = article.id.link;
     getId("fecha").innerHTML = article.id.fechaLanzado;
 });
-
+var refRespuestas = database.ref().child('respuestas');
+refRespuestas.on('child_added', function(ss) {
+    var respuesta = ss.val();
+    var item = document.createElement('li');
+    item.innerHTML = '<input name="respuesta" type="radio" value="' + respuesta.valor + '"> ' + respuesta.respuesta;
+    getId('lista').appendChild(item);
+});
